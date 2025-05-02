a090a16762d8bb38f7644d66a97f1f83f8f3619a
QHongboQ <zhou979675497@gmail.com>
2025-05-02 12:04:23 -0700

    Remove auto-generated out/ folder from tracking

--- out/build/GCC 14.2.0 x86_64-w64-mingw32 (ucrt64)/_deps/flac-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/NoSystemFile/Hongbo/VSProject/Minesweeper_Backtracking/out/build/GCC 14.2.0 x86_64-w64-mingw32 (ucrt64)/_deps/flac-src
-# Build directory: C:/NoSystemFile/Hongbo/VSProject/Minesweeper_Backtracking/out/build/GCC 14.2.0 x86_64-w64-mingw32 (ucrt64)/_deps/flac-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("microbench")
